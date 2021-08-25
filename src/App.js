0d4323767ddb37eef4a0162175d0f324f6183b35
unknown <mahtasham20@gmail.com>
2021-08-25 01:03:46 -0700

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ListItems from './ListItems'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
+
+
+
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+   
+    if(newItem.text){
+      const newItems=[...this.state.items,newItem];
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+
+      })  
+      toast.success("item added successfully")  
+      // alert("item added")
+    }
+
+  }
+  deleteItem(key){
+    const filteredItems = this.state.items.filter(item =>
+      item.key!==key);
+      this.setState({
+      items:filteredItems
+    })
+    toast.warning("item deleted successfully")
+    // alert("item deleted")
+  }
+  setUpdate(text, key){
+    const items = this.state.items;
+    items.map(item =>{
+      if(item.key===key){
+        item.text=text;
+      }
+
+    })
+    this.setState({
+      Items: items
+    })
+    // alert("item updated")
+    toast.info("item updated successfully")
+  }
+  
+  render(){
+    return(
+      <div className="App">
+      <header>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter Text"
+          value={this.state.currentItem.text}
+          onChange={this.handleInput}/>
+          <button type="submit"
+          
+          >Add</button>
+
+          {/* <ToastContainer/> */}
+        </form>
       </header>
-    </div>
-  );
+      <ListItems items ={this.state.items}
+      DeleteItem = {this.deleteItem}
+      setUpdate ={this.setUpdate}></ListItems>
+      </div>
+    )
+
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
+   <ToastContainer />
     <App />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
